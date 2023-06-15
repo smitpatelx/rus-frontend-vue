cce4d5f973851e52897f562448a0398b8e54a99e
smitpatelx <smitbpatel1999@gmail.com>
2023-06-15 18:19:58 -0500

    Fixed vue3 notification build errors.

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import Notifications from '@kyvg/vue3-notification';
+import velocity from 'velocity-animate';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(VueQueryPlugin);
-app.use(Notifications);
+app.use(Notifications, {
+  velocity,
+});
 
 app.mount('#vueApp');
