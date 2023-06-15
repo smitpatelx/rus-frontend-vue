fdb4c5485c94efd7c4ab2279729436f081dc69fc
smitpatelx <smitbpatel1999@gmail.com>
2023-06-15 16:51:53 -0500

    Added vue3-notification.

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import './style/main.scss';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import Notifications from '@kyvg/vue3-notification';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(VueQueryPlugin);
+app.use(Notifications);
 
 app.mount('#vueApp');
