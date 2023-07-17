8ebd72829450b2a12a0ac76ad160008eace9f966
smitpatelx <smitbpatel1999@gmail.com>
2023-07-17 03:21:49 -0500

    Eslint config updated.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,8 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    // '@vue/eslint-config-prettier/skip-formatting'
-    '@vue/eslint-config-prettier',
+    '@vue/eslint-config-prettier/skip-formatting',
+    // '@vue/eslint-config-prettier',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -16,19 +16,19 @@ module.exports = {
   rules: {
     'prettier/prettier': [
       'error',
-      {
-        tabWidth: 2,
-        singleQuote: true,
-        printWidth: 100,
-        trailingComma: 'all',
-        semi: true,
-        arrowParens: 'always',
-        jsxBracketSameLine: true,
-        bracketSpacing: true,
-        bracketSameLine: false,
-        singleAttributePerLine: true,
-        embeddedLanguageFormatting: 'off',
-      },
+      // {
+      //   tabWidth: 2,
+      //   singleQuote: true,
+      //   printWidth: 100,
+      //   trailingComma: 'all',
+      //   semi: true,
+      //   arrowParens: 'always',
+      //   jsxBracketSameLine: true,
+      //   bracketSpacing: true,
+      //   bracketSameLine: false,
+      //   singleAttributePerLine: true,
+      //   embeddedLanguageFormatting: 'off',
+      // },
     ],
   },
 };
